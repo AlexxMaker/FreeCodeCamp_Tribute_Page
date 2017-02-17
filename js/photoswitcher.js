6f6ba85a9399d3cef04a0919358db8a32332a3e8
AlexxMaker <Alexeus92@mail.ru>
2017-02-17 15:48:28 +0200

    Image switcher working without fade

--- js/photoswitcher.js
+++ js/photoswitcher.js
@@ -1,25 +1,19 @@
-/** $(document).ready(function(){
-	var $imgs = $(".fader").find("img"),
-    i = 0;
 
-	function changeImage(){
-    var next = (++i % $imgs.length);
-    $($imgs.get(next - 1)).fadeOut(2000);
-    $($imgs.get(next)).fadeIn(1000);
-}
-var interval = setInterval(changeImage, 7000);
-}); **/
-
-$(document).ready(function () {
-    
-        setTimeout(function() {
-        for (var i = 1; i <= 3; i++) {
-        var image = "./img/main" + i + ".jpg";
-        console.log(image);
-        var x = document.getElementsByClassName("fader");
-        x.src = image;
-        console.log(x.src);
+window.onload = function () {
+            var rotator = document.getElementById("rotator");
+            var images = rotator.getElementsByTagName("img");
+            for (var i = 1; i < images.length; i++) {
+                images[i].style.display = "none";
+            }
+            var counter = 1;
+            setInterval(function () {
+                for (var i = 0; i < images.length; i++) {
+                    images[i].style.display = "none";
+                }
+                images[counter].style.display = "inline";
+                counter++;
+                if (counter == images.length) {
+                    counter = 0;
+                }
+            }, 5000);
         };
-    }, 5000);
-});
-
